8850b3bea06c6a992361afcf2d7798067258fc88
dscrane <daegancrane@gmail.com>
2020-11-03 09:02:28 -0500

    finished fizzbuzz exercise

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,36 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+/* SOLUTION 1 */
+function fizzBuzz(n) {
+  for (let i=1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      // (i % 15 === 0) can also be used here as the if conditional
+      console.log('fizzbuzz')
+    } else if (i % 3 !== 0 && i % 5 !== 0) {
+      console.log(i)
+    } else {
+      i % 3 === 0 ? console.log('fizz') : console.log('buzz')
+    }
+  }
+}
+/* ==== */
+
+/* SOLUTION 2 */
+// Much codier and less impressive but can show you know your simple stuff pretty well
+// const fizzBuzz = n => {
+//   for (let i = 0; i < n; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//       console.log('FizzBuzz');
+//     } else if (i % 3 === 0) {
+//       console.log('Fizz');
+//     } else if (i % 5 === 0) {
+//       console.log('Buzz');
+//     } else {
+//       console.log(i);
+//     }
+//   }
+// };
+/* ==== */
 
 module.exports = fizzBuzz;
