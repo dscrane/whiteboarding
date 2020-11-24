ed347b81740c8d627a9e44b0182774cea14006f3
dscrane <daegancrane@gmail.com>
2020-11-24 10:36:39 -0500

    finished the stack and qfroms exercises

--- exercises/stack-complete/index.js
+++ exercises/stack-complete/index.js
@@ -10,6 +10,22 @@
 //   s.pop(); // returns 2
 //   s.pop(); // returns 1
 
-class Stack {}
+class Stack {
+  constructor() {
+    this.data = [];
+  }
+
+  push(el) {
+    this.data.unshift(el);
+  }
+
+  pop() {
+    return this.data.shift();
+  }
+
+  peek() {
+    return this.data[0];
+  }
+}
 
 module.exports = Stack;
